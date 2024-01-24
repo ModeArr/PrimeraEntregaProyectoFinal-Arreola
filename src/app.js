7ecc60615d9af522564df17ce1e6f49c9f71ffb4
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2024-01-23 23:21:07 -0600

    Add Routes fix

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const productsRoutes = require("./routes/products.routes")
+const cartsRoutes = require("./routes/carts.routes")
+
+PORT = 8080
+
+const app = express()
+
+app.use(express.urlencoded({ extends: true }));
+app.use(express.json()); 
+app.use(express.static(__dirname + '/public'))
+
+app.use(`/${API_PREFIX}/products`, productsRoutes)
+app.use(`/${API_PREFIX}/carts`, cartsRoutes)
+
+app.listen(PORT, () => {
+    console.log("SERVER FUNCIONANDO")
+})
